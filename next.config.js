eca103c62cf97d60c178ee49104075f3f3a6a8d9
Jaehun <qkdwogns98@gmail.com>
2023-04-15 16:55:23 +0900

    mdx file testing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+/** @see https://nextjs.org/docs/advanced-features/using-mdx */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+
+    // MDX Provider
+    // providerImportSource: '@mdx-js/react',
+  },
+})
+
+module.exports = withMDX(nextConfig)
